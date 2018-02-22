5803a4345bbac92e896a006769da546b67638b1d
Dan Ladendorf <dladendorf@gmail.com>
2018-02-21 20:32:29 -0700

    Done testing. Deployed. Ready to do front-end.

---
+++ ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'silent move armed find pipe render lift below mean aware picture feel',
+  'https://rinkeby.infura.io/BT5aNdzdatRBPJwy52wl'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0], gasLimit: '10000000' });
+
+  //console.log(interface);
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
